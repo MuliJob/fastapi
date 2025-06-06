7c272e65d599fd96fe02b4603d08cfd25e633d52
MuliJob <munyoki912@gmail.com>
2025-06-06 17:13:56 +0300

    :sparkles: Query parameter

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 """Main file"""
+from typing import Optional
 from fastapi import FastAPI
 
 
 app = FastAPI()
 
 
-@app.get('/')
-def index():
+@app.get('/blog')
+def index(limit = 10, published: bool = True, sort: Optional[str] = None):
     """Index route function"""
-    return {'data': 'blog list'}
+    if published:
+        return {'data': f'{limit} published blogs from the db'}
+    else:
+        return {'data': f'{limit} blogs from the db'}
 
 
 @app.get('/blog/unpublished')
